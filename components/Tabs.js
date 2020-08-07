7c4578bac9ed89f511c1fd2c80ab1e75192e6d42
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-08-07 11:32:19 -0400

    added trending topics

--- components/Tabs.js
+++ components/Tabs.js
@@ -1,3 +1,6 @@
+const { default: Axios } = require("axios")
+
+import axios from 'axios'
 // STEP 2: Create tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-api.herokuapp.com/topics
@@ -9,3 +12,26 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+const topics = document.querySelector('.topics')
+
+const tab = (topic) =>{
+    const topicDiv = document.createElement('div')
+    topicDiv.classList.add('tab')
+    topicDiv.textContent = topic
+    topics.appendChild(topicDiv)
+}
+
+axios.get('https://lambda-times-api.herokuapp.com/topics')
+    .then(response => {
+        console.log(response)
+        const datas = response.data.topics
+        datas.forEach(topic => {
+            tab(topic)
+            console.log('hi')
+        });
+        // debugger
+    })
+    .catch(error => {
+        debugger
+    })
+
